23ed5375c56ccf10eec3c32748b1b4068fb2559d
ghouse7867 <128900693+ghouse7867@users.noreply.github.com>
2023-05-18 17:02:51 +0000

    dist

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+  },
+  
+  build: {
+    outDir: 'dist', // Output directory for the built files
+    assetsDir: 'assets', // Directory for static assets
+    // You can add more build options as needed
   }
 })
