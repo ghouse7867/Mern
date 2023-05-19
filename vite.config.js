4bbfd8030558e1408a82b3b20841ff58bc207a33
ghouse7867 <128900693+ghouse7867@users.noreply.github.com>
2023-05-19 09:12:32 +0000

    vites

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-  },build: {
-    outDir: 'dist', // Output directory for the built files
-    assetsDir: 'assets', // Directory for static assets
-    // You can add more build options as needed
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    optimizeDeps: false, // Disable dependency pre-bundling
   },
   rollupOptions: {
-      input: {
-        main: './src/index.jsx' // Specify your main entry file here
-      }
+    input: {
+      main: './src/index.jsx'
+    },
   }
 })
