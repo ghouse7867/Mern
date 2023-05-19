2e18389e4009f2b6ca31de77eea48c15c3cdb0cf
ghouse7867 <128900693+ghouse7867@users.noreply.github.com>
2023-05-19 10:30:48 +0000

    cors

--- Server/Apps.cjs
+++ Server/Apps.cjs
@@ -16,7 +16,11 @@ const corsOptions ={
 }
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
-
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+})
 app.use(express.json());
 
 app.use(authrouter)
