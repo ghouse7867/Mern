1892c85b6033be2a332c6d28bae083bb458a6461
ghouse7867 <128900693+ghouse7867@users.noreply.github.com>
2023-05-19 10:37:14 +0000

    credentials

--- Server/Apps.cjs
+++ Server/Apps.cjs
@@ -9,18 +9,14 @@ require('./db/conn.cjs');
 
 // Enable CORS
 const cors=require("cors");
-const corsOptions ={
-   origin:'https://meek-bonbon-3f6c68.netlify.app', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
-app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-})
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://meek-bonbon-3f6c68.netlify.app');,
+  withCredentials: true,
+  next();
+});
+
 app.use(express.json());
 
 app.use(authrouter)
