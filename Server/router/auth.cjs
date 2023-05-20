e214d164e50605d9a7a12c5a39dd624229eb9f81
ghouse7867 <128900693+ghouse7867@users.noreply.github.com>
2023-05-20 17:18:30 +0000

    await

--- Server/router/auth.cjs
+++ Server/router/auth.cjs
@@ -73,7 +73,7 @@ router.post('/signin', async (req, res) => {
       const token = await userLogin.generateAuthToken();
        console.log(token)
        
-       res.cookie("jwtoken", token, {
+      await res.cookie("jwtoken", token, {
          expires:new Date(Date.now() + 25892000000),
          httpOnly:true,
          secure: true,
