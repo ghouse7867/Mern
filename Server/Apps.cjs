98067807ef39fe87008f327601481e56c1a37c29
ghouse7867 <128900693+ghouse7867@users.noreply.github.com>
2023-05-21 12:23:26 +0000

    cors

--- Server/Apps.cjs
+++ Server/Apps.cjs
@@ -15,6 +15,14 @@ app.use(cors({
   domain: "https://mernm.onrender.com",
   credentials: true
 }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://meek-bonbon-3f6c68.netlify.app");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
+
+
 
 app.use(express.json());
 
