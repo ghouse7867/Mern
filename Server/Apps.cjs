665c6309de046abcb4ecd3105c1fbb3cd21bca88
ghouse7867 <128900693+ghouse7867@users.noreply.github.com>
2023-05-22 06:38:40 +0000

    Access-Control-Allow-Credentials

--- Server/Apps.cjs
+++ Server/Apps.cjs
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://meek-bonbon-3f6c68.netlify.app");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // Added this line
   next();
 });
 
