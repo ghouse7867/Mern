b5842a39a65b48167c59b16590e6ea833079dbb7
ghouse7867 <128900693+ghouse7867@users.noreply.github.com>
2023-05-23 06:46:18 +0000

    []

--- Server/router/auth.cjs
+++ Server/router/auth.cjs
@@ -145,7 +145,7 @@ router.get('/logout', (req, res) => {
   });
 
   // Remove tokens from the user document
-  req.cookies.jwtoken = "";
+  req.cookies.jwtoken = [];
 
   // Save the updated user document
   req.rootUser.save();
