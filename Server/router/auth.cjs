1533bd6c32755c58b076e4f250adae555c4d7533
ghouse7867 <128900693+ghouse7867@users.noreply.github.com>
2023-05-23 08:14:29 +0000

    try again

--- Server/router/auth.cjs
+++ Server/router/auth.cjs
@@ -79,7 +79,7 @@ router.post('/signin', async (req, res) => {
    
        console.log(res.getHeaders())
      if(!isMatch) {
-       res.status(400).json({error :" Invalid Credentials"})
+       res.status(400).json({error :" Invalid Credentials try again"})
        window.alert(error)
      } else {
         res.json({message:" user Signed in successfully"})
@@ -87,6 +87,7 @@ router.post('/signin', async (req, res) => {
        
      }else {
        res.json({ error :" Invalid Credentials"})
+       window.alert(error)
      }
      
    } catch(err) {
