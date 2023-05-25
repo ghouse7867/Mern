53212c9304503d67ba2f02a8d77288781754a1ed
ghouse7867 <128900693+ghouse7867@users.noreply.github.com>
2023-05-25 05:23:50 +0000

    useContext

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 // import '../App.css'
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
 import {UserContext} from "../App.jsx"
